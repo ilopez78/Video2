469b79610ccd484087d94769cd90e67be40548f1
ilopez78 <99553835+ilopez78@users.noreply.github.com>
2022-07-15 22:13:56 -0600

    Agregar archivos de proyecto.

---
+++ Video2/Services/CameraService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+
+namespace Video2.Services
+{
+    public class CameraService
+    {
+        PermissionStatus cameraOK;
+        PermissionStatus storageOK;
+
+        public async Task Init()
+        {
+            await CrossMedia.Current.Initialize();
+
+            cameraOK = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Camera);
+            storageOK = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Storage);
+
+            if (cameraOK != PermissionStatus.Granted || storageOK != PermissionStatus.Granted)
+            {
+                var status = await CrossPermissions.Current.RequestPermissionsAsync(new[] { Permission.Camera, Permission.Storage });
+
+                cameraOK = status[Permission.Camera];
+                storageOK = status[Permission.Storage];
+            }
+        }
+
+       
+
+
+
+        public async Task<MediaFile> TakeVideo()
+        {
+            if (cameraOK == PermissionStatus.Granted
+                && storageOK == PermissionStatus.Granted
+                && CrossMedia.Current.IsTakeVideoSupported)
+            {
+                var options = new StoreVideoOptions { SaveToAlbum = true };
+                var file = await CrossMedia.Current.TakeVideoAsync(options);
+                return file;
+            }
+
+            return null;
+        }
+
+        public async Task<MediaFile> ChooseVideo()
+        {
+            if (CrossMedia.Current.IsPickVideoSupported)
+            {
+                var file = await CrossMedia.Current.PickVideoAsync();
+                return file;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Video2/ViewModels/MenuViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Video2.ViewModels
+{
+    public class MenuViewModel : BaseViewModel
+    {
+        public ICommand NavigateCommand { private set; get; }
+
+        public INavigation Navigation { get; set; }
+
+        public MenuViewModel(INavigation navigation)
+        {
+            Navigation = navigation;
+            NavigateCommand = new Command<Type>(async (pageType) => await Navigate(pageType));
+        }
+
+        async Task Navigate(Type pageType)
+        {
+            var page = (Page)Activator.CreateInstance(pageType);
+            await Navigation.PushAsync(page);
+        }
+    }
+}
